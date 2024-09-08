9fb5e65b5458c680e9163de0f53237bc865c1494
Moayad Shloul <shloul97@gmail.com>
2024-09-09 01:09:18 +0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASS', '');
-define('DB_DATABASE', 'conf_grad');
+define('DB_DATABASE', '');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASS,DB_DATABASE);
 
 session_start();
